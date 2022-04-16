7ab7de747286846464bb952ee72969f5d4fb8d97
Gabriel Spindola <gspindola@magnasistemas.com.br>
2022-04-16 20:34:54 -0300

    feat: Adicionando Checked-List

--- src/components/ListItem/styles.ts
+++ src/components/ListItem/styles.ts
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const Container = styled.div``;
+type ContainerProps = {
+  done: boolean;
+};
+
+export const Container = styled.div(
+  ({ done }: ContainerProps) =>
+    `
+    display: flex;
+    background-color: ${done ? '#330099' : '#20212C'};
+    padding: 10px;
+    border-radius: 10px;
+    margin-bottom: 10px;
+    align-items: center;
+
+    input {
+        width: 25px;
+        height: 25px;
+        margin-right: 10px;
+    }
+
+    label {
+        font-size: 24px;
+        font-weight: 700;
+        color: #FFF;
+        text-decoration: ${done ? 'line-through' : 'initial'};
+    }
+`
+);
