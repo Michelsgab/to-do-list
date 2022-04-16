8f5c131ab23fd712249afe6c6932f668117aeba8
Gabriel Spindola <gspindola@magnasistemas.com.br>
2022-04-16 20:18:27 -0300

    feat: to-do-list

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { ListItem } from "./components/ListItem";
 
-function App() {
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: "Estudar JavaScript", done: false },
+    { id: 2, name: "Estudar React.js", done: false },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>To Do List</C.Header>
+        {list.map((item, index) => (
+          <ListItem key={index} item={item} />
+        ))}
+      </C.Area>
+    </C.Container>
   );
-}
+};
 
 export default App;
